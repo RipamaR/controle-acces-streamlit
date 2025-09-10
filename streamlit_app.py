11455626faff5beb2b1d4bc0b3ddacf8b4f8859b
RipamaR <103128815+RipamaR@users.noreply.github.com>
2025-09-10 05:14:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -387,7 +387,7 @@ def draw_combined_graph(components_1, adj_1, labels_1,
         if si is not None and di is not None:
             net.add_edge(base_idx + si, base_idx + di, arrows="to")
 
-    _pyvis_show(net, height=1000, width=1800)
+    _pyvis_show(net, height="1000px", width="100%")
 
 # =============== PROPAGATION RBAC (fichiers) =================
 def propagate_rbac_from_excel(df: pd.DataFrame) -> pd.DataFrame:
